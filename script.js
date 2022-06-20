c0918ae300b75c42d3229bad1d0ef62260955029
10Sadique <jafarsjahan@gmail.com>
2022-06-20 22:40:09 +0600

    update to dates

--- script.js
+++ script.js
@@ -12,14 +12,14 @@ const account1 = {
     pin: 1111,
 
     movementsDates: [
-        '2019-11-18T21:31:17.178Z',
-        '2019-12-23T07:42:02.383Z',
-        '2020-01-28T09:15:04.904Z',
-        '2020-04-01T10:17:24.185Z',
-        '2020-05-08T14:11:59.604Z',
-        '2020-05-27T17:01:17.194Z',
-        '2020-07-11T23:36:17.929Z',
-        '2020-07-12T10:51:36.790Z',
+        '2021-11-18T21:31:17.178Z',
+        '2021-12-23T07:42:02.383Z',
+        '2022-03-28T09:15:04.904Z',
+        '2022-04-01T10:17:24.185Z',
+        '2022-04-22T14:11:59.604Z',
+        '2022-06-13T17:01:17.194Z',
+        '2022-06-15T23:36:17.929Z',
+        '2022-06-19T10:51:36.790Z',
     ],
     currency: 'EUR',
     locale: 'pt-PT', // de-DE
@@ -32,14 +32,14 @@ const account2 = {
     pin: 2222,
 
     movementsDates: [
-        '2019-11-01T13:15:33.035Z',
-        '2019-11-30T09:48:16.867Z',
-        '2019-12-25T06:04:23.907Z',
-        '2020-01-25T14:18:46.235Z',
-        '2020-02-05T16:33:06.386Z',
-        '2020-04-10T14:43:26.374Z',
-        '2020-06-25T18:49:59.371Z',
-        '2020-07-26T12:01:20.894Z',
+        '2021-11-13T13:15:33.035Z',
+        '2021-12-24T09:48:16.867Z',
+        '2021-12-31T06:04:23.907Z',
+        '2022-01-25T14:18:46.235Z',
+        '2022-02-05T16:33:06.386Z',
+        '2022-04-10T14:43:26.374Z',
+        '2022-05-25T18:49:59.371Z',
+        '2022-06-18T12:01:20.894Z',
     ],
     currency: 'USD',
     locale: 'en-US',
@@ -79,25 +79,44 @@ const inputClosePin = document.querySelector('.form__input--pin')
 //////////////////////////////////////////
 // Functionality of the bankist app
 
-// Displaying movements
-const displayMovements = function (acc, sort = false) {
-    containerMovements.innerHTML = ''
+// Movement Dates
+const formatMovementDate = function (date) {
+    const calcDaysPassed = (date1, date2) =>
+        Math.round(Math.abs(date2 - date1) / (1000 * 24 * 60 * 60))
 
-    const movs = sort
-        ? acc.movements.slice().sort((a, b) => a - b)
-        : acc.movements // Sorting of movements array
+    const daysPassed = calcDaysPassed(new Date(), date)
+    console.log(daysPassed)
 
-    movs.forEach((mov, i) => {
-        const type = mov > 0 ? 'deposit' : 'withdrawal'
-
-        const date = new Date(acc.movementsDates[i])
+    if (daysPassed === 0) return 'Today'
+    if (daysPassed === 1) return 'Yesterday'
+    if (daysPassed <= 7) return `${daysPassed} days ago`
+    else {
         const now = new Date()
         const day = `${date.getDate()}`.padStart(2, 0)
         const month = `${date.getMonth() + 1}`.padStart(2, 0)
         const year = date.getFullYear()
 
-        const displayDate = `${day}/${month}/${year}`
+        return `${day}/${month}/${year}`
+    }
+}
 
+// Displaying movements
+const displayMovements = function (acc, sort = false) {
+    containerMovements.innerHTML = ''
+
+    // Sorting the movements array
+    const movs = sort
+        ? acc.movements.slice().sort((a, b) => a - b)
+        : acc.movements
+
+    movs.forEach((mov, i) => {
+        const type = mov > 0 ? 'deposit' : 'withdrawal'
+
+        // Date functionality
+        const date = new Date(acc.movementsDates[i])
+        const displayDate = formatMovementDate(date)
+
+        // Printing movements to the App
         const html = `
           <div class="movements__row">
             <div class="movements__type movements__type--${type}">${
